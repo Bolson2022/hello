84accb41d395c0c4bd24b428c2ba1b45d5fabd97
Ben Olson <benolson@unomaha.edu>
2022-08-25 18:53:49 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Benjamin Olson
+ *         bolson21@unl.edu
+ * Date: 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("Benjamin Olson!\n");
+
+  return 0;
+}
